9a7e1776a467ea17ea6dd42cfc255dad2a73889e
慕龍 <2069893427@qq.com>
2022-09-15 22:01:35 +0800

    完成CheckboxAll组件

---
+++ src/components/CheckboxAll.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mu-Checkbox_all">
+    <!-- 全选按钮 -->
+    <div class="mu-Checkbox_checkAll">
+      <slot name="checkAll"></slot>
+    </div>
+    <!-- 选项 -->
+    <div class="mu-Checkbox_options">
+      <slot name="options"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Mu-CheckboxAll' // 复选框全选
+}
+</script>
+
+<style lang="scss" scoped>
+.mu-Checkbox_options{
+  margin-top: 10px;
+}
+</style>
